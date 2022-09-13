12a16b44da17989e82753f7c087fddebc8afcd8b
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-09-13 16:06:32 +0000

    css y js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+let draw = document.querySelector("#draw");
+let sort = document.querySelector("#sort");
+let generateCards = document.getElementById("generateCards");
+let bubbleLog = document.getElementById("bubbleLog");
+let listaCartas = [];
+
+// Generar Pinta y Numero
+let randomCard = () => {
+  let Pinta = ["spades", "hearts", "diamonds", "cubs"];
+  let Numero = Math.floor(Math.random() * 12) + 1;
+  let pintaRandom = Math.floor(Math.random() * Pinta.length);
+  let datos = {
+    number: Numero,
+    pinta: Pinta[pintaRandom]
+  };
+  listaCartas.push(datos);
+  return content;
+};
